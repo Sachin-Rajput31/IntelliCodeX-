284ae8739329554900bd90a5fb4735b2f51f4347
SachinRajput31 <sachingirase2004@gmail.com>
2025-04-19 09:34:07 +0530

    fix: updated mongoose connection with env variable

--- backend/server.js
+++ backend/server.js
@@ -6,12 +6,13 @@ const app = require("./app");
 const PORT = process.env.PORT || 3000;
 
 // Add MongoDB connection
-mongoose.connect(process.env.MONGO_URI)
+mongoose
+  .connect(process.env.MONGODB_URI)
   .then(() => console.log("✅ Connected to MongoDB"))
-  .catch(err => console.error("❌ MongoDB connection error:", err));
+  .catch((err) => console.error("❌ MongoDB connection error:", err));
 
 console.log("✅ API Key Loaded:", process.env.GOOGLE_GEMINI_KEY ? "Yes" : "No");
 
 app.listen(PORT, () => {
-    console.log(`🚀 Server running on port ${PORT}`);
+  console.log(`🚀 Server running on port ${PORT}`);
 });
